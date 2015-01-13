bf2cedd09531228dc7ff1bebafc682e71a6e6d69
Xavier Ho <contact@xavierho.com>
2015-01-13 14:06:14 +1100

    Scrapped new sites

--- scraper.py
+++ scraper.py
@@ -19,7 +19,9 @@ This program requires BeautifulSoup and Requests.
 import os
 import csv
 import bs4
+import json
 import pprint
+import urllib
 import requests
 import collections
 
@@ -39,30 +41,61 @@ def get_games():
   # pprint.pprint(games, indent=2)
   return games
 
+def compile_urls(cached=True):
+  path = os.path.join(__dir, 'generated', 'roguelike-games.json')
+  if cached and os.path.exists(path):
+    with open(path) as f:
+      return json.loads(f.read())
+
+  games = get_games()
+
+  print "Compiling a list of URLS...."
+  for i, game in enumerate(games.values()):
+    print u'{}%: {}'.format(i*100/len(games), game['Title'].decode('utf-8'))
+    game['Links'] = [x.encode('utf-8').replace('"', '\\"') for x in get_urls(game)]
+
+  # with open(path, 'w+') as f:
+  #   f.write(json.dumps(games), indent=2)
+  return games
+
+def compile_content():
+  games = compile_urls(False)
+  content = {}
+  for i, (title, game) in enumerate(games.items()):
+    print u'{}%: {}'.format(i*100/len(games), title.decode('utf-8'))
+    content[title] = get_url_content(game)
+
+    with open(os.path.join(__dir, 'generated', 'roguelike-game-articles.json'), 'w+') as f:
+      f.write(json.dumps(content, indent=2))
+
 def get_urls(game):
   '''Return a list of potential websites to scrap'''
   title = game['Title'].replace(' ', '+')
   developer = game['Developer'].replace(' ', '+')
-  response = requests.get('http://duckduckgo.com/html/?q={}+{}'.format(title, developer))
+  response = requests.get('http://duckduckgo.com/html/?q={}'.format(urllib.quote("{} {}".format(title, developer))), timeout=(9.1, 12.1))
   soup = bs4.BeautifulSoup(response.text)
   links = []
   for node in soup.select('div.web-result'):
     if 'web-result-sponsored' in node['class']:
       continue
-    links.append(node.select('a.large')[0].get('href'))
+    try:
+      links.append(node.select('a.large')[0].get('href'))
+    except:
+      pass
   return links
 
-def compile_urls():
-  games = get_games()
-  with open(os.path.join(__dir, 'generated', 'postmortems-with-developer.csv'), 'w+') as f:
-    f.write('Title,Links\n')
-    total = len(games)
-    for i, game in enumerate(games.values()):
-      print '{}%: {}'.format(i*100/total, game['Title'])
-      game['Links'] = [x.encode('utf-8').replace('"', '\\"') for x in get_urls(game)]
-      f.write('"{}","{}"\n'.format(game['Title'], '\n'.join(game['Links'])))
-
+def get_url_content(game):
+  scrape = {}
+  for i, url in enumerate(game['Links']):
+    print "{}/{}\tLoading {}".format(i+1, len(game['Links']), url)
+    try:
+      response = requests.get(url, timeout=(3.1, 10.1))
+    except Exception as e:
+      print "{}: Failed to load {}".format(e, url)
+      continue
+    scrape[url] = response.text
+  return scrape
 
 if '__main__' in __name__:
-  compile_urls()
+  compile_content()
 
