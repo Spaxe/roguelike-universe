6a96029a1fde701562f2c4aea4a20d58804a54f9
Xavier Ho <contact@xavierho.com>
2015-12-23 00:36:02 +1100

    testing connection successful

--- devops/populate_database.js
+++ devops/populate_database.js
@@ -1,14 +1,20 @@
 'use strict';
 
-const r = require("rethinkdb");
+const r = require('rethinkdb');
 
-r.connect( { host: 'localhost', port: 8005 }, function(err, conn) {
-    if (err) {
-      console.error(`Connection failed. Did you remember to establish ssh tunnel?
+r.connect({ host: 'localhost', port: 8005 })
+  .then( connection => {
 
-        runner tunnel_driver
-      `);
-      throw err;
-    }
-    console.log('Connection established.');
-})
+    console.log('Connection successful!');
+    connection.close();
+
+  }).catch( err => {
+
+    console.error(`Connection failed. Did you remember to establish ssh tunnel?
+
+      runner start_tunnel_driver
+    `);
+    throw err;
+
+  });
+
--- devops/test_connection.js
+++ devops/test_connection.js
@@ -2,13 +2,19 @@
 
 const r = require('rethinkdb');
 
-r.connect( { host: 'localhost', port: 8005 }, function(err, conn) {
-    if (err) {
-      console.error(`Connection failed. Did you remember to establish ssh tunnel?
+r.connect({ host: 'localhost', port: 8005 })
+  .then( connection => {
 
-        runner tunnel_driver
-      `);
-      throw err;
-    }
-    console.log('connection successful!');
-})
+    console.log('Connection successful!');
+    connection.close();
+
+  }).catch( err => {
+
+    console.error(`Connection failed. Did you remember to establish ssh tunnel?
+
+      runner start_tunnel_driver
+    `);
+    throw err;
+
+  });
+
