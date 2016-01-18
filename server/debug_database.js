2c1d3cb451a5769bd1856a3dea6938c5831c4540
Xavier Ho <contact@xavierho.com>
2016-01-19 10:43:15 +1100

    gh-pages clean up

--- server/debug_database.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const r = require('rethinkdb');
-const async = require('asyncawait/async');
-const await = require('asyncawait/await');
-
-let c = await (r.connect({host: 'database', port: 28015}));
