c7fac48b91ace091edfa9d4cdbe284973907065e
Xavier Ho <contact@xavierho.com>
2016-06-22 21:37:19 +1000

    Drawing arcs again for the 4th time

--- web/endpoint.js
+++ web/endpoint.js
@@ -1,42 +1,44 @@
-import gs from 'google-spreadsheet';
+import SpreadSheet from 'google-spreadsheet-reader';
 
-let doc_roguelike = new gs('1iov1Vh-rjbv4rhVScDaP6aZBW3_hCijDpLpP8HRxyVU');
-// let doc_video_game = new gs('1Em1ugWpRuRdguxK0A5qFY1C7gPZsjntrP_FWZte_dNc');
-// let doc_relations = new gs('14-6ZXSABKd8p_1tc9wZiOET7MY5x5krQ0PPBGFyf004');
+module.exports = {
 
-// const version = '0.1.0';
+  roguelikes: () => {
 
-// export let api = (resource, parameters='') => {
-//   const endpoint = `https://tonicdev.io/spaxe/roguelike-universe-gs/${version}/${resource}/${parameters}`;
-//   let xhrPromise = new XMLHttpRequestPromise();
-//   return xhrPromise.send({
-//     method: 'GET',
-//     url: endpoint
-//   });
-// };
+    let gs_roguelike = new SpreadSheet('1iov1Vh-rjbv4rhVScDaP6aZBW3_hCijDpLpP8HRxyVU');
 
-export let roguelikes = () => {
+    return gs_roguelike.load().then( res => {
 
-  return new Promise( (resolve, reject) => {
+      let sheet_name = Object.keys(res)[0];
+      return res[sheet_name];
 
-    doc_roguelike.getRows( 1, (err, row) => {
-      if (err) {
-        reject({ error: 'error', message: err });
-      } else {
-        resolve({ status: 'ok', data: row.map(r => cleanup(r)) });
-      }
     });
 
-  });
+  },
 
-};
+  videogames: () => {
 
-let cleanup = (doc_result) => {
-    if (doc_result._xml)
-        delete doc_result._xml;
-    if (doc_result.id)
-        delete doc_result.id;
-    if (doc_result._links)
-        delete doc_result._links;
-    return doc_result;
+    let gs_videogames = new SpreadSheet('1Em1ugWpRuRdguxK0A5qFY1C7gPZsjntrP_FWZte_dNc');
+
+    return gs_videogames.load().then( res => {
+
+      let sheet_name = Object.keys(res)[0];
+      return res[sheet_name];
+
+    });
+
+  },
+
+  relations: () => {
+
+    let gs_relations = new SpreadSheet('14-6ZXSABKd8p_1tc9wZiOET7MY5x5krQ0PPBGFyf004');
+
+    return gs_relations.load().then( res => {
+
+      let sheet_name = Object.keys(res)[0];
+      return res[sheet_name];
+
+    });
+
+  }
+
 };
--- web/roguelike-universe.js
+++ web/roguelike-universe.js
@@ -1,13 +1,89 @@
 import d3 from 'd3';
+import _ from 'lodash';
 import endpoint from './endpoint';
 
-endpoint.roguelikes().then((res) => {
 
-  d3.select('#roguelike-relations')
-    .text(JSON.stringify(res));
+let margin = {top: 20, right: 20, bottom: 20, left: 20},
+    width = 500 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-}).catch(e => {
+let svg = d3.select('#roguelike-relations')
+            .append('svg')
+            .attr('class', 'relations')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom);
 
-  console.error(e);
+let frame = svg.append('g')
+               .attr("transform", `translate(${margin.left},${margin.top})`);
+
+let x = d3.time.scale()
+    .range([0, width]);
+
+let arc = d3.svg.arc()
+                .startAngle(Math.PI/2)
+                .endAngle(-Math.PI/2);
+
+let xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('middle');
+
+//////////////////////////
+
+let roguelikes, relations;
+
+endpoint.roguelikes().then(data => {
+
+  roguelikes = data;
+
+  roguelikes = roguelikes.map(r => {
+    r.year = Number(r.year);
+    return r;
+  });
+
+  x.domain(d3.extent(roguelikes, function(d) { return new Date(d.year, 1, 1); }));
+
+  frame.append('g')
+       .attr('class', 'x axis')
+       .attr('transform', `translate(0,${height/2})`)
+       .call(xAxis);
+
+  return endpoint.relations();
+
+}).then(data => {
+
+  relations = data;
+
+  relations.forEach(r => {
+
+    let game = _.filter(roguelikes, { title: r.title });
+    if (!game.length) {
+      console.warn(`${rtitle} is not in the database. Please add it at https://docs.google.com/spreadsheets/d/1iov1Vh-rjbv4rhVScDaP6aZBW3_hCijDpLpP8HRxyVU/edit?usp=drive_web`);
+    };
+    game = game[0];
+
+    delete r.title;
+    for (let title of _.values(r)) {
+      let other_game = _.filter(roguelikes, { title });
+      if (!other_game.length) continue;
+      other_game = other_game[0];
+
+      let x0 = x(new Date(game.year, 1, 1));
+      let x1 = x(new Date(other_game.year, 1, 1));
+      let r = Math.abs(x1-x0)/2;
+
+      arc.innerRadius(r)
+         .outerRadius(r);
+
+      frame.append('path')
+           .attr('class', 'relation-arc')
+           .attr('transform', `translate(${(x0+x1)/2},${height/2})`)
+           .attr('d', arc);
+
+    }
+
+  });
 
 });
+
+
+
