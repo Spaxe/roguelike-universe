6884d7c6660b8ecba1b576c9d2b46eda577ad8d0
Xavier Ho <contact@xavierho.com>
2016-06-22 21:55:18 +1000

    Drawing bottom arcs

--- web/roguelike-universe.js
+++ web/roguelike-universe.js
@@ -2,10 +2,9 @@ import d3 from 'd3';
 import _ from 'lodash';
 import endpoint from './endpoint';
 
-
 let margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 700 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
 
 let svg = d3.select('#roguelike-relations')
             .append('svg')
@@ -17,11 +16,10 @@ let frame = svg.append('g')
                .attr("transform", `translate(${margin.left},${margin.top})`);
 
 let x = d3.time.scale()
-    .range([0, width]);
+    .range([0, width])
+    .domain([new Date(1960, 1, 1), new Date(2019, 1, 1)]);
 
-let arc = d3.svg.arc()
-                .startAngle(Math.PI/2)
-                .endAngle(-Math.PI/2);
+let arc = d3.svg.arc();
 
 let xAxis = d3.svg.axis()
     .scale(x)
@@ -29,23 +27,22 @@ let xAxis = d3.svg.axis()
 
 //////////////////////////
 
-let roguelikes, relations;
+let roguelikes, relations, videogames;
 
 endpoint.roguelikes().then(data => {
 
   roguelikes = data;
 
-  roguelikes = roguelikes.map(r => {
-    r.year = Number(r.year);
-    return r;
-  });
-
-  x.domain(d3.extent(roguelikes, function(d) { return new Date(d.year, 1, 1); }));
-
   frame.append('g')
        .attr('class', 'x axis')
        .attr('transform', `translate(0,${height/2})`)
        .call(xAxis);
+
+  return endpoint.videogames();
+
+}).then(data => {
+
+  videogames = data;
 
   return endpoint.relations();
 
@@ -63,21 +60,52 @@ endpoint.roguelikes().then(data => {
 
     delete r.title;
     for (let title of _.values(r)) {
+
       let other_game = _.filter(roguelikes, { title });
-      if (!other_game.length) continue;
-      other_game = other_game[0];
 
-      let x0 = x(new Date(game.year, 1, 1));
-      let x1 = x(new Date(other_game.year, 1, 1));
-      let r = Math.abs(x1-x0)/2;
+      if (other_game.length) {
 
-      arc.innerRadius(r)
-         .outerRadius(r);
+        other_game = other_game[0];
 
-      frame.append('path')
-           .attr('class', 'relation-arc')
-           .attr('transform', `translate(${(x0+x1)/2},${height/2})`)
-           .attr('d', arc);
+        var x0 = x(new Date(Number(game.year), 1, 1));
+        var x1 = x(new Date(Number(other_game.year), 1, 1));
+        var r = Math.abs(x1-x0)/2;
+
+        arc.startAngle(Math.PI/2)
+           .endAngle(-Math.PI/2)
+           .innerRadius(r)
+           .outerRadius(r);
+
+        frame.append('path')
+             .attr('class', 'relation-arc')
+             .attr('transform', `translate(${(x0+x1)/2},${height/2})`)
+             .attr('d', arc);
+
+      } else {
+
+        other_game = _.filter(videogames, { title });
+
+        if (other_game.length) {
+
+          other_game = other_game[0];
+
+          var x0 = x(new Date(Number(game.year), 1, 1));
+          var x1 = x(new Date(Number(other_game.year), 1, 1));
+          var r = Math.abs(x1-x0)/2;
+
+          arc.startAngle(-Math.PI/2)
+             .endAngle(-3*Math.PI/2)
+             .innerRadius(r)
+             .outerRadius(r);
+
+          frame.append('path')
+               .attr('class', 'relation-arc')
+               .attr('transform', `translate(${(x0+x1)/2},${height/2+25})`)
+               .attr('d', arc);
+
+        }
+
+      }
 
     }
 
