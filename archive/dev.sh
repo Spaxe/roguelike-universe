e317c4367caa9db23d9083e140521d87db8abd51
Xavier Ho <contact@xavierho.com>
2018-08-10 00:37:47 +1000

    Archiving old versions

--- archive/dev.sh
+++
@@ -1,7 +0,0 @@
-node_modules/.bin/watchify roguelike-relations.js \
-  -v -d \
-  -t [ babelify --presets [ es2015 ] ] \
-  -p [ minifyify --map roguelike-relations.map --output public/roguelike-relations.map ] \
-  -o public/roguelike-relations.min.js &
-
-cd public ; python3 -m http.server 8005
