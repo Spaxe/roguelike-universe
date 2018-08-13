552c4cb586bb1cb024573915de6d065df7f3c156
Xavier Ho <contact@xavierho.com>
2018-08-13 10:21:52 +1000

    Fixed Diablo II link in timeline

--- roguelike-utils.js
+++ roguelike-utils.js
@@ -34,7 +34,7 @@ function gatherInfluence (files) {
       r.Influences.forEach(i => {
         if (validYears(r.Name, i)) {
           // Temporary patching games out of roguelike list
-          if (i === 'Diablo' || i === 'Spelunky' || i === 'The Binding of Isaac') {
+          if (i === 'Diablo' || i === 'Diablo II' || i === 'Spelunky' || i === 'The Binding of Isaac') {
             influences.push({
               titleA: r.Name, titleB: i, yearA: releasedYears[r.Name], yearB: releasedYears[i],
               categoryA: 'roguelike', categoryB: 'roguelikelike', type: 'known',
@@ -81,7 +81,7 @@ function gatherInfluence (files) {
       r.Influences.forEach(i => {
         if (validYears(r.Name, i)) {
           // Temporary patching Spelunky out of roguelike list
-          if (i === 'Spelunky') {
+          if (i === 'Spelunky' || i === 'Diablo II') {
             influences.push({
               titleA: r.Name, titleB: i, yearA: releasedYears[r.Name], yearB: releasedYears[i],
               categoryA: 'roguelikelike', categoryB: 'roguelikelike',  type: 'known',
